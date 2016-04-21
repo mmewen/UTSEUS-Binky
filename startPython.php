ebf6f4fa541fbc49d63d9296b2ddc6ae2f8053e7
Mewen MICHEL <mewenmichel@yahoo.fr>
2016-04-21 19:06:36 +0800

    On its way...

--- startPython.php
+++ startPython.php
@@ -1,8 +1,11 @@
 <?php
-   exec('sudo python /home/pi/Desktop/Binky/UTSEUS-Binky/grovepi/scan.py');
+	$cmd = 'python /home/pi/Desktop/Binky/UTSEUS-Binky/grovepi/scan.py';
+	echo shell_exec(sprintf("nohup %s > %s & echo $!", $cmd, "/tmp/binkyScan"));
+
 /*
 You must put this in /etc/sudoers :
 apache ALL = NOPASSWD: /sbin/halt /usr/bin/python
 (or)
 www-data ALL = NOPASSWD: /sbin/halt /usr/bin/python
+*/
 ?>
